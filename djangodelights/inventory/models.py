578cceb435dcf7fb4fab79ef554ec294f232cd86
django master <72372841+mohamahrez@users.noreply.github.com>
2022-03-03 08:06:00 +0000

    v2

--- djangodelights/inventory/models.py
+++ djangodelights/inventory/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+  name = models.CharField(max_length=30,null=True,blank=True,unique=True)
+  quantity = models.FloatField(null=True, blank=True, default=0.0)
+  unit = models.CharField(max_length=30,null=True,blank=True)
+  unit_price = models.FloatField(null=True, blank=True, default=0.0)
+'''   def get_absolute_url(self):
+        return "/ingredients"'''
+
+
+
+
+class MenuItem(models.Model):
+  title=  models.CharField(max_length=30,null=True,blank=True,unique=True)
+  price = models.FloatField(null=True, blank=True, default=0.0)
+'''     def get_absolute_url(self):
+        return "/menu"'''
+
+
+class RecipeRequirement(models.Model):
+  menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+  ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+  quantity = models.FloatField(null=True, blank=True, default=0.0)
+
+
+class Purchase(models.Model):
+  
+  menu_item = models.ForeignKey(MenuItem,on_delete=models.CASCADE)
+  timestamp = models.DateTimeField(auto_now=True)
+
+
+'''   def get_absolute_url(self):
+        return "/purchases"
+'''
+  
